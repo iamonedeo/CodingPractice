08fe95ea235efe3ee981c1439f5e307a6e1b3044
iamonedeo <131924902+iamonedeo@users.noreply.github.com>
2023-05-09 09:45:53 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.c
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.c
@@ -5,21 +5,14 @@
 // numbers_len은 배열 numbers의 길이입니다.
 int solution(int numbers[], size_t numbers_len) {
     int answer = 0;
-    int max1=0, max2=0;
-    int maxnumb;
-    for(int i=0; i<numbers_len;i++)
+ 
+    for(int i=0;i<numbers_len-1;i++)
     {
-        if(max1<numbers[i])
+        for(int j=i+1;j<numbers_len;j++)
         {
-            max1=numbers[i];
-            maxnumb=i;
+            if(answer<numbers[i]*numbers[j])
+                answer=numbers[i]*numbers[j];
         }
     }
-    //2개의 수가 같은 경우
-    for(int i=0; i<numbers_len;i++)
-    {
-        if(i==maxnumb);
-        else if(max2<numbers[i]) max2=numbers[i];
-    }
-    return max1*max2;
+    return answer;
 }
