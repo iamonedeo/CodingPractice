3e029ef4f77a3069a35cf8db2f643add2a73a5af
iamonedeo <131924902+iamonedeo@users.noreply.github.com>
2023-05-09 15:01:30 +0900

    [unrated] Title: 문자열을 정수로 변환하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.c
+++ 프로그래머스/unrated/181848. 문자열을 정수로 변환하기/문자열을 정수로 변환하기.c
@@ -5,19 +5,19 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* n_str) {
     int answer = 0;
-    int jari=1;
-    
-        for (int i = 0; i < strlen(n_str);i++)
+    int jari = 1;
+    int len;
+
+    for (len = 0; n_str[len];len++) {};
+
+    char* pstr = n_str;
+    pstr += len-1;
+
+    for (int i = 0; i < strlen(n_str);i++)
     {
+        answer += jari * (*(pstr-i)-'0');
         jari *= 10;
     }
-    jari /= 10;
-
-    for (int i = 0;i < strlen(n_str);i++)
-    {
-        answer += (n_str[i] - '0') * jari;
-        jari /= 10;
-    }
-
+    
     return answer;
 }
