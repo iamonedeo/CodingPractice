c388ff131d9bf197c96bb36b5e1df2409967ba1c
iamonedeo <131924902+iamonedeo@users.noreply.github.com>
2023-07-14 21:12:53 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,24 +1,11 @@
-#include <iostream>
-
-int main()
-{
-    using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+int n,x,a[10005];
+int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int* ary;
-    int N, X;
-    cin>>N>>X;
-    
-    ary=new int[N];
-    
-    for(int i=0; i<N;i++)
-    {
-        cin>>ary[i];
-        if(ary[i]<X) cout<<ary[i]<<" ";
-    }
-    
-    delete[] ary;
-    
-    return 0;
+    cin>>n>>x;
+    for(int i=0; i<n;i++)cin>>a[i];
+    for(int i=0; i<n;i++)
+        if(a[i]<x)cout<<a[i]<<' ';
 }
