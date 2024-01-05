342d3b26c572eb40685e5260b1e1f0ccc0ad1f32
iamonedeo <131924902+iamonedeo@users.noreply.github.com>
2024-01-05 13:18:52 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,29 +1,30 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    using namespace std;
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    
-    int N;
-    cin>>N;
-    int num[10]={0,};
-    
-    while(N!=0)
-    {
-        num[N%10]++;
-        N/=10;
-    }
-    num[6]+=num[9];
-    num[6]%2!=0?num[6]++:0;
-    num[6]/=2;
-    
-    int max=num[0];
-    for(int i=1; i<9;i++)
-        if(num[i]>max) max=num[i];
-    
-    cout<<max;
-    
-    return 0;
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	int array[10] = {};
+	int n;
+	cin >> n;
+	while (n > 0)
+	{
+		array[n % 10]++;
+		n /= 10;
+	}
+
+	array[6] += array[9];
+	array[9] = 0;
+	if (array[6] % 2 == 0) array[6] /= 2;
+	else array[6] = array[6] / 2 + 1;
+
+	int max=0;
+	for (int i = 0; i < 10; i++)
+	{
+		if (array[i] > max) max = array[i];
+	}
+	cout << max;
 }
